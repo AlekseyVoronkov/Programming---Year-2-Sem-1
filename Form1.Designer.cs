6e1134636569eef3d2c5e5e731fd740371ea81f1
AlekseyVoronkov <144922364+AlekseyVoronkov@users.noreply.github.com>
2024-09-09 01:42:52 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Year2Sem1Lab0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.countButton = new System.Windows.Forms.Button();
+            this.cubeVolume = new System.Windows.Forms.Label();
+            this.sphereVolume = new System.Windows.Forms.Label();
+            this.vastedMaterial = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.studentsList = new System.Windows.Forms.RichTextBox();
+            this.displayStudentsButton = new System.Windows.Forms.Button();
+            this.textBoxGroup = new System.Windows.Forms.TextBox();
+            this.textBoxGrade = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxA.Location = new System.Drawing.Point(152, 8);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 0;
+            this.textBoxA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxA_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите ребро a -\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(18, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите R -";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(11, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Объем куба -";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(11, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Объем шара -";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(11, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "% Материала в отходы -";
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(152, 34);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(100, 20);
+            this.textBoxR.TabIndex = 6;
+            this.textBoxR.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxR_KeyPress);
+            // 
+            // countButton
+            // 
+            this.countButton.Location = new System.Drawing.Point(168, 60);
+            this.countButton.Name = "countButton";
+            this.countButton.Size = new System.Drawing.Size(75, 23);
+            this.countButton.TabIndex = 7;
+            this.countButton.Text = "Посчитать";
+            this.countButton.UseVisualStyleBackColor = true;
+            this.countButton.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // cubeVolume
+            // 
+            this.cubeVolume.AutoSize = true;
+            this.cubeVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cubeVolume.Location = new System.Drawing.Point(121, 95);
+            this.cubeVolume.Name = "cubeVolume";
+            this.cubeVolume.Size = new System.Drawing.Size(50, 16);
+            this.cubeVolume.TabIndex = 8;
+            this.cubeVolume.Text = "label6";
+            // 
+            // sphereVolume
+            // 
+            this.sphereVolume.AutoSize = true;
+            this.sphereVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sphereVolume.Location = new System.Drawing.Point(123, 125);
+            this.sphereVolume.Name = "sphereVolume";
+            this.sphereVolume.Size = new System.Drawing.Size(50, 16);
+            this.sphereVolume.TabIndex = 9;
+            this.sphereVolume.Text = "label7";
+            // 
+            // vastedMaterial
+            // 
+            this.vastedMaterial.AutoSize = true;
+            this.vastedMaterial.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vastedMaterial.Location = new System.Drawing.Point(202, 151);
+            this.vastedMaterial.Name = "vastedMaterial";
+            this.vastedMaterial.Size = new System.Drawing.Size(50, 16);
+            this.vastedMaterial.TabIndex = 10;
+            this.vastedMaterial.Text = "label8";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.ItemSize = new System.Drawing.Size(56, 18);
+            this.tabControl1.Location = new System.Drawing.Point(-5, -1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.RightToLeftLayout = true;
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(291, 231);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.PeachPuff;
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.textBoxA);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.vastedMaterial);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.sphereVolume);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.cubeVolume);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.countButton);
+            this.tabPage1.Controls.Add(this.textBoxR);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(283, 205);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Задание 1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.PeachPuff;
+            this.tabPage2.Controls.Add(this.studentsList);
+            this.tabPage2.Controls.Add(this.displayStudentsButton);
+            this.tabPage2.Controls.Add(this.textBoxGroup);
+            this.tabPage2.Controls.Add(this.textBoxGrade);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(283, 205);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Задание 2";
+            // 
+            // studentsList
+            // 
+            this.studentsList.Location = new System.Drawing.Point(12, 98);
+            this.studentsList.Name = "studentsList";
+            this.studentsList.Size = new System.Drawing.Size(230, 92);
+            this.studentsList.TabIndex = 5;
+            this.studentsList.Text = "";
+            // 
+            // displayStudentsButton
+            // 
+            this.displayStudentsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.displayStudentsButton.Location = new System.Drawing.Point(12, 60);
+            this.displayStudentsButton.Name = "displayStudentsButton";
+            this.displayStudentsButton.Size = new System.Drawing.Size(229, 32);
+            this.displayStudentsButton.TabIndex = 4;
+            this.displayStudentsButton.Text = "Display Students";
+            this.displayStudentsButton.UseVisualStyleBackColor = true;
+            this.displayStudentsButton.Click += new System.EventHandler(this.displayStudentsButton_Click);
+            // 
+            // textBoxGroup
+            // 
+            this.textBoxGroup.Location = new System.Drawing.Point(141, 34);
+            this.textBoxGroup.Name = "textBoxGroup";
+            this.textBoxGroup.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGroup.TabIndex = 3;
+            // 
+            // textBoxGrade
+            // 
+            this.textBoxGrade.Location = new System.Drawing.Point(142, 8);
+            this.textBoxGrade.Name = "textBoxGrade";
+            this.textBoxGrade.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGrade.TabIndex = 2;
+            this.textBoxGrade.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxGrade_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(9, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 16);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Задайте группу";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(9, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Задайте оценку";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(284, 223);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.Button countButton;
+        private System.Windows.Forms.Label cubeVolume;
+        private System.Windows.Forms.Label sphereVolume;
+        private System.Windows.Forms.Label vastedMaterial;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBoxGroup;
+        private System.Windows.Forms.TextBox textBoxGrade;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox studentsList;
+        private System.Windows.Forms.Button displayStudentsButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace Year2Sem1Lab0
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> students = new List<Student>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateData();
+        }
+
+        double cubeA = 0;
+        double sphereR = 0;
+
+        private void textBoxA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBoxR_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void countButton_Click(object sender, EventArgs e)
+        {
+            if (textBoxA.Text == "")
+            {
+                textBoxA.Text = "0";
+            }
+
+            if (textBoxR.Text == "")
+            {
+                textBoxR.Text = "0";
+            }
+
+            cubeA = Convert.ToDouble(textBoxA.Text, CultureInfo.InvariantCulture);
+            sphereR = Convert.ToDouble(textBoxR.Text, CultureInfo.InvariantCulture);
+
+            cubeVolume.Text = Convert.ToString(Math.Pow(cubeA, 3));
+            double cubeV = Convert.ToDouble(cubeVolume.Text);
+
+            sphereVolume.Text = Convert.ToString( (4 / 3 * Math.PI * Math.Pow(sphereR, 3)) );
+            double sphereV = Convert.ToDouble(sphereVolume.Text);
+
+            vastedMaterial.Text = Convert.ToString( (1 - sphereV / cubeV) * 100 );
+        }
+
+
+        private void GenerateData()
+        {
+            students.Add(new Student { LastName = "Smith", FirstName = "John", Group = "A1", Grade = 90 });
+            students.Add(new Student { LastName = "Doe", FirstName = "Jane", Group = "A1", Grade = 85 });
+            students.Add(new Student { LastName = "Brown", FirstName = "Emily", Group = "B2", Grade = 90 });
+            students.Add(new Student { LastName = "Petrenko", FirstName = "Egor", Group = "A1", Grade = 90 });
+            students.Add(new Student { LastName = "Voronkov", FirstName = "Aleksey", Group = "A1", Grade = 1 });
+            students.Add(new Student { LastName = "Adolf", FirstName = "Hitler", Group = "B2", Grade = 90 });
+            SaveDataToJson();
+        }
+
+        private void SaveDataToJson()
+        {
+            var jsonData = JsonConvert.SerializeObject(students);
+            File.WriteAllText(Application.StartupPath + "\\students.json", jsonData);
+        }
+
+        private void LoadDataFromJson()
+        {
+            var jsonData = File.ReadAllText(Application.StartupPath + "\\students.json");
+            students = JsonConvert.DeserializeObject<List<Student>>(jsonData);
+        }
+
+        private void textBoxGrade_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void displayStudentsButton_Click(object sender, EventArgs e)
+        {
+            if (textBoxGrade.Text == "")
+            {
+                textBoxGrade.Text = "0";
+            }
+
+            if (textBoxGroup.Text == "")
+            {
+                textBoxGroup.Text = "0";
+            }
+
+            string group = textBoxGroup.Text;
+            int grade = Convert.ToInt32(textBoxGrade.Text);
+            var filteredStudents = students.Where(s => s.Group == group && s.Grade == grade).ToList();
+
+            studentsList.Clear();
+
+            foreach (var student in filteredStudents)
+            {
+                studentsList.AppendText($"{student.FirstName} {student.LastName} - Group: {student.Group}, Grade: {student.Grade}\r\n");
+            }
+
+            if (studentsList.Text.Length == 0) 
+            {
+                studentsList.AppendText("По заданным данным студентов не найдено");
+            }
+
+        }
+    }
+
+    [Serializable]
+    public class Student
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Group { get; set; }
+        public int Grade { get; set; }
+    }
+}
