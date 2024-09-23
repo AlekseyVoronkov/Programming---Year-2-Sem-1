482923f4a142739fcffec353ecfab88fb315f998
AlekseyVoronkov <144922364+AlekseyVoronkov@users.noreply.github.com>
2024-09-23 17:00:50 +0700

    First iteration of this abomination has been created

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace DichotomyMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtE = new System.Windows.Forms.TextBox();
+            this.txtFunction = new System.Windows.Forms.TextBox();
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.plot1 = new OxyPlot.WindowsForms.PlotView();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rtbAnswers = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(80, 9);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 0;
+            this.txtA.Text = "Пример: -5";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(80, 35);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 1;
+            this.txtB.Text = "Пример: 5";
+            // 
+            // txtE
+            // 
+            this.txtE.Location = new System.Drawing.Point(80, 61);
+            this.txtE.Name = "txtE";
+            this.txtE.Size = new System.Drawing.Size(100, 20);
+            this.txtE.TabIndex = 2;
+            this.txtE.Text = "Пример: 0,0001";
+            // 
+            // txtFunction
+            // 
+            this.txtFunction.Location = new System.Drawing.Point(80, 87);
+            this.txtFunction.Name = "txtFunction";
+            this.txtFunction.Size = new System.Drawing.Size(100, 20);
+            this.txtFunction.TabIndex = 3;
+            this.txtFunction.Text = "Пример x*3";
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.AutoSize = true;
+            this.BtnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnCalculate.Location = new System.Drawing.Point(220, 70);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(204, 37);
+            this.BtnCalculate.TabIndex = 4;
+            this.BtnCalculate.Text = "Метод дихотомии";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "a =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 26);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "b =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "e =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 26);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "f(x) =";
+            // 
+            // plot1
+            // 
+            this.plot1.BackColor = System.Drawing.SystemColors.Control;
+            this.plot1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.plot1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.plot1.Location = new System.Drawing.Point(0, 138);
+            this.plot1.Name = "plot1";
+            this.plot1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plot1.Size = new System.Drawing.Size(746, 317);
+            this.plot1.TabIndex = 0;
+            this.plot1.Text = "plot1";
+            this.plot1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plot1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plot1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(220, 48);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 20);
+            this.txtStart.TabIndex = 10;
+            this.txtStart.Text = "Пример: -10";
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(326, 48);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 20);
+            this.txtEnd.TabIndex = 11;
+            this.txtEnd.Text = "Пример: 10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(222, 1);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Начало   |   Конец";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(274, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Отрезка";
+            // 
+            // rtbAnswers
+            // 
+            this.rtbAnswers.Location = new System.Drawing.Point(432, 9);
+            this.rtbAnswers.Name = "rtbAnswers";
+            this.rtbAnswers.Size = new System.Drawing.Size(302, 98);
+            this.rtbAnswers.TabIndex = 14;
+            this.rtbAnswers.Text = "";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(746, 455);
+            this.Controls.Add(this.rtbAnswers);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.plot1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCalculate);
+            this.Controls.Add(this.txtFunction);
+            this.Controls.Add(this.txtE);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtE;
+        private System.Windows.Forms.TextBox txtFunction;
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private OxyPlot.WindowsForms.PlotView plot1;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox rtbAnswers;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using OxyPlot;
+using OxyPlot.Series;
+using org.mariuszgromada.math.mxparser;
+using System.Collections.Generic;
+
+namespace DichotomyMethod
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<DataPoint> Graphic = new List<DataPoint>();
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GetFunction(txtFunction.Text);
+                DichotomyMethod();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter valid numbers.");
+            }
+        }
+
+        public void GetFunction(string functionText)
+        {
+            Graphic.Clear();
+
+            Function function = new Function("f(x) = " + functionText);
+
+            for (int counterI = Convert.ToInt32(txtStart.Text); counterI < Convert.ToInt32(txtEnd.Text); ++counterI)
+            {
+                Expression e1 = new Expression($"f({counterI})", function);
+                Graphic.Add(new DataPoint(counterI, e1.calculate()));
+            }
+
+            var plotModel = new PlotModel { Title = "График функции f(x)" };
+
+            var medianLine = new LineSeries
+            {
+                Title = "X",
+                Color = OxyColor.FromRgb(0, 0, 0),
+                StrokeThickness = 2
+            };
+
+            medianLine.Points.Add(new DataPoint(Convert.ToInt32(txtStart.Text), 0));
+            medianLine.Points.Add(new DataPoint(Convert.ToInt32(txtEnd.Text), 0));
+
+            var absicc = new LineSeries
+            {
+                Title = "Y",
+                Color = OxyColor.FromRgb(0, 0, 0), // Красный цвет
+                StrokeThickness = 2,
+            };
+
+            absicc.Points.Add(new DataPoint(0, Convert.ToInt32(txtStart.Text)));
+            absicc.Points.Add(new DataPoint(0, Convert.ToInt32(txtEnd.Text)));
+
+            // Создаем серию точек графика
+            var lineSeries = new LineSeries
+            {
+                Title = "f(x)",
+                Color = OxyColor.FromRgb(255, 0, 0), // Синий цвет линии
+                LineStyle = LineStyle.Dot
+            };
+
+            // Добавляем все точки в серию
+            lineSeries.Points.AddRange(Graphic);
+
+            // Добавляем серию точек к модели графика
+            plotModel.Series.Add(lineSeries);
+            plotModel.Series.Add(medianLine);
+            plotModel.Series.Add(absicc);
+
+            // Отображаем график
+            plot1.Model = plotModel;
+        }
+
+        public void DichotomyMethod()
+        {
+            Function func = new Function("f(x) = " + txtFunction.Text);
+
+            double fa = SolveFunc(func, txtA.Text);
+            double fb = SolveFunc(func, txtB.Text);
+
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+            double eps = Convert.ToDouble(txtE.Text);
+
+            if (fa * fb > 0)
+            {
+                MessageBox.Show("The function must have opposite signs at the endpoints of the interval.");
+            }
+
+            while (b - a > eps)
+            {
+                double c = (a + b) / 2;
+                double fc = SolveFunc(func, c.ToString().Replace(",", "."));
+
+                if (Math.Abs(fc) == 0)
+                {
+                    MessageBox.Show($"Ответ: {c}");
+                    return;
+                }
+                else if (fa * fc < 0)
+                {
+                    b = c;
+                }
+                else
+                {
+                    a = c;
+                    fa = fc;
+                }
+            }
+            rtbAnswers.AppendText($"Ответ: {(a + b) / 2}\r\n");
+            MessageBox.Show($"Ответ: {(a + b) / 2}");
+            return;
+        }
+
+        public double SolveFunc(Function function, string x)
+        {
+            return new Expression($"f({x})", function).calculate();
+        }
+
+    }
+}
