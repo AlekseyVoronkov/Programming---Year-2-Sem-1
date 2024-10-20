306da1fa1d1ec72fc91876cc42af9bbb7bdf93ca
AlekseyVoronkov <144922364+AlekseyVoronkov@users.noreply.github.com>
2024-10-21 00:36:11 +0700

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace NewtonMethod
+namespace NewtonMethod
 {
     partial class Form1
     {
@@ -42,12 +42,15 @@
             this.расчетыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tbAcc = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.посторитьГрафикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // rtbAnswers
             // 
-            this.rtbAnswers.Location = new System.Drawing.Point(388, 34);
+            this.rtbAnswers.Location = new System.Drawing.Point(382, 36);
             this.rtbAnswers.Name = "rtbAnswers";
             this.rtbAnswers.Size = new System.Drawing.Size(364, 96);
             this.rtbAnswers.TabIndex = 21;
@@ -55,7 +58,7 @@
             // 
             // tbEpsilon
             // 
-            this.tbEpsilon.Location = new System.Drawing.Point(85, 103);
+            this.tbEpsilon.Location = new System.Drawing.Point(79, 105);
             this.tbEpsilon.Name = "tbEpsilon";
             this.tbEpsilon.Size = new System.Drawing.Size(100, 20);
             this.tbEpsilon.TabIndex = 19;
@@ -64,7 +67,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(30, 106);
+            this.label5.Location = new System.Drawing.Point(24, 108);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(41, 13);
             this.label5.TabIndex = 18;
@@ -73,7 +76,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(191, 71);
+            this.label4.Location = new System.Drawing.Point(185, 73);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(89, 13);
             this.label4.TabIndex = 17;
@@ -82,7 +85,7 @@
             // 
             // tbB
             // 
-            this.tbB.Location = new System.Drawing.Point(294, 68);
+            this.tbB.Location = new System.Drawing.Point(288, 70);
             this.tbB.Name = "tbB";
             this.tbB.Size = new System.Drawing.Size(74, 20);
             this.tbB.TabIndex = 16;
@@ -90,7 +93,7 @@
             // 
             // tbA
             // 
-            this.tbA.Location = new System.Drawing.Point(111, 68);
+            this.tbA.Location = new System.Drawing.Point(105, 70);
             this.tbA.Name = "tbA";
             this.tbA.Size = new System.Drawing.Size(74, 20);
             this.tbA.TabIndex = 15;
@@ -99,7 +102,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 71);
+            this.label3.Location = new System.Drawing.Point(16, 73);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 13);
             this.label3.TabIndex = 14;
@@ -109,7 +112,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 37);
+            this.label1.Location = new System.Drawing.Point(38, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(24, 13);
             this.label1.TabIndex = 13;
@@ -117,7 +120,7 @@
             // 
             // tbFunc
             // 
-            this.tbFunc.Location = new System.Drawing.Point(85, 34);
+            this.tbFunc.Location = new System.Drawing.Point(79, 36);
             this.tbFunc.Name = "tbFunc";
             this.tbFunc.Size = new System.Drawing.Size(100, 20);
             this.tbFunc.TabIndex = 12;
@@ -141,6 +144,7 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.посторитьГрафикToolStripMenuItem,
             this.расчетыToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
@@ -157,7 +161,7 @@
             // 
             // tbAcc
             // 
-            this.tbAcc.Location = new System.Drawing.Point(294, 103);
+            this.tbAcc.Location = new System.Drawing.Point(288, 105);
             this.tbAcc.Name = "tbAcc";
             this.tbAcc.Size = new System.Drawing.Size(74, 20);
             this.tbAcc.TabIndex = 23;
@@ -166,18 +170,49 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(191, 91);
+            this.label2.Location = new System.Drawing.Point(185, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(97, 39);
             this.label2.TabIndex = 24;
             this.label2.Text = "Точность\r\n(кол-во знаков  ->\r\nпосле запятой)";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Корень",
+            "Минимум",
+            "Максимум"});
+            this.checkedListBox1.Location = new System.Drawing.Point(279, 15);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(83, 49);
+            this.checkedListBox1.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(194, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Что ищем - ";
+            // 
+            // посторитьГрафикToolStripMenuItem
+            // 
+            this.посторитьГрафикToolStripMenuItem.Name = "посторитьГрафикToolStripMenuItem";
+            this.посторитьГрафикToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.посторитьГрафикToolStripMenuItem.Text = "Посторить График";
+            this.посторитьГрафикToolStripMenuItem.Click += new System.EventHandler(this.посторитьГрафикToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(746, 455);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tbAcc);
             this.Controls.Add(this.rtbAnswers);
@@ -219,6 +254,9 @@
         private System.Windows.Forms.ToolStripMenuItem расчетыToolStripMenuItem;
         private System.Windows.Forms.TextBox tbAcc;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ToolStripMenuItem посторитьГрафикToolStripMenuItem;
     }
 }
 
