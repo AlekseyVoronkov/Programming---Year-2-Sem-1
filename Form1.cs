26c0929ffaf2d891f0ce449ae4beb322245c1851
AlekseyVoronkov <144922364+AlekseyVoronkov@users.noreply.github.com>
2024-10-31 16:58:18 +0700

    fixed version

--- Form1.cs
+++ Form1.cs
@@ -1,135 +1,151 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 using System.Drawing;
 using OxyPlot;
 using OxyPlot.Series;
 using org.mariuszgromada.math.mxparser;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.InteropServices;
 
 namespace DichotomyMethod
 {
-
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    public Form1()
     {
-        public Form1()
+      InitializeComponent();
+    }
+
+    private List<DataPoint> Graphic = new List<DataPoint>();
+
+    public void GetFunction(string functionText)
+    {
+      double interval = Math.Abs(Convert.ToDouble(tbA.Text)) + Math.Abs(Convert.ToDouble(tbB.Text));
+      Graphic.Clear();
+
+      Function function = new Function("f(x) = " + functionText);
+
+      var plotModel = new PlotModel { Title = "График функции f(x)" };
+
+      var medianLine = new LineSeries
+      {
+        Title = "X",
+        Color = OxyColor.FromRgb(0, 0, 0),
+        StrokeThickness = 2
+      };
+
+      medianLine.Points.Add(new DataPoint(-interval, 0));
+      medianLine.Points.Add(new DataPoint(interval, 0));
+
+      var absicc = new LineSeries
+      {
+        Title = "Y",
+        Color = OxyColor.FromRgb(0, 0, 0),
+        StrokeThickness = 2,
+      };
+
+      absicc.Points.Add(new DataPoint(0, interval));
+      absicc.Points.Add(new DataPoint(0, -interval));
+
+      // Создаем серию точек графика
+      var lineSeries = new LineSeries
+      {
+        Title = "f(x)",
+        Color = OxyColor.FromRgb(255, 0, 0),
+      };
+
+      // why
+      string ihatemxparser = "";
+      for (double counterI = Convert.ToDouble(tbA.Text) * 10; counterI < Convert.ToDouble(tbB.Text) * 10 + 1; ++counterI)
+      {
+        ihatemxparser = (counterI / 10).ToString().Replace(",", ".");
+
+        Expression e1 = new Expression($"f({ihatemxparser})", function);
+        double y = e1.calculate();
+        lineSeries.Points.Add(new DataPoint(Convert.ToDouble(ihatemxparser.Replace(".", ",")), y));
+      }
+
+      lineSeries.Points.AddRange(Graphic);
+
+      plotModel.Series.Add(lineSeries);
+      plotModel.Series.Add(medianLine);
+      plotModel.Series.Add(absicc);
+
+      plot1.Model = plotModel;
+    }
+
+
+    public void DichotomyMethod()
+    {
+      Function func = new Function("f(x) = " + tbFunc.Text);
+
+      double fa = SolveFunc(func, tbA.Text);
+      double fb = SolveFunc(func, tbB.Text);
+
+      double a = Convert.ToDouble(tbA.Text);
+      double b = Convert.ToDouble(tbB.Text);
+      double eps = Convert.ToDouble(tbEpsilon.Text.Replace(".", ","));
+
+     if (fa * fb > 0)
+      {
+        MessageBox.Show(" f (a) * f (b) must have 0 in the interval [a,b]\nRigth answer is not guaranteed.");
+      }
+
+      while (b - a > eps)
+      {
+        double c = (a + b) / 2;
+        double fc = SolveFunc(func, c.ToString().Replace(",", "."));
+
+        if (Math.Abs(fc) == 0)
         {
-            InitializeComponent();
+          MessageBox.Show($"Ответ: {c}");
+          return;
         }
-
-        private List<DataPoint> Graphic = new List<DataPoint>();
-
-        private void BtnCalculate_Click(object sender, EventArgs e)
+        else if (fa * fc < 0)
         {
-            try
-            {
-                GetFunction(txtFunction.Text);
-                DichotomyMethod();
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Please enter valid numbers.");
-            }
+          b = c;
         }
-
-        public void GetFunction(string functionText)
+        else
         {
-            Graphic.Clear();
-
-            Function function = new Function("f(x) = " + functionText);
-
-            for (int counterI = Convert.ToInt32(txtStart.Text); counterI < Convert.ToInt32(txtEnd.Text); ++counterI)
-            {
-                Expression e1 = new Expression($"f({counterI})", function);
-                Graphic.Add(new DataPoint(counterI, e1.calculate()));
-            }
-
-            var plotModel = new PlotModel { Title = "График функции f(x)" };
-
-            var medianLine = new LineSeries
-            {
-                Title = "X",
-                Color = OxyColor.FromRgb(0, 0, 0),
-                StrokeThickness = 2
-            };
-
-            medianLine.Points.Add(new DataPoint(Convert.ToInt32(txtStart.Text), 0));
-            medianLine.Points.Add(new DataPoint(Convert.ToInt32(txtEnd.Text), 0));
-
-            var absicc = new LineSeries
-            {
-                Title = "Y",
-                Color = OxyColor.FromRgb(0, 0, 0), // Красный цвет
-                StrokeThickness = 2,
-            };
-
-            absicc.Points.Add(new DataPoint(0, Convert.ToInt32(txtStart.Text)));
-            absicc.Points.Add(new DataPoint(0, Convert.ToInt32(txtEnd.Text)));
-
-            // Создаем серию точек графика
-            var lineSeries = new LineSeries
-            {
-                Title = "f(x)",
-                Color = OxyColor.FromRgb(255, 0, 0), // Синий цвет линии
-                LineStyle = LineStyle.Dot
-            };
-
-            // Добавляем все точки в серию
-            lineSeries.Points.AddRange(Graphic);
-
-            // Добавляем серию точек к модели графика
-            plotModel.Series.Add(lineSeries);
-            plotModel.Series.Add(medianLine);
-            plotModel.Series.Add(absicc);
-
-            // Отображаем график
-            plot1.Model = plotModel;
+          a = c;
+          fa = fc;
         }
+      }
 
-        public void DichotomyMethod()
-        {
-            Function func = new Function("f(x) = " + txtFunction.Text);
+      double answer = (a + b) / 2;
+      rtbAnswers.AppendText($"Ответ: {Math.Round(answer, Convert.ToInt32(tbAcc.Text.Replace(".", ".")))}\n");
+      return;
+    }
 
-            double fa = SolveFunc(func, txtA.Text);
-            double fb = SolveFunc(func, txtB.Text);
+    public double SolveFunc(Function function, string x)
+    {
+      return new Expression($"f({x})", function).calculate();
+    }
 
-            double a = Convert.ToDouble(txtA.Text);
-            double b = Convert.ToDouble(txtB.Text);
-            double eps = Convert.ToDouble(txtE.Text);
-
-            if (fa * fb > 0)
-            {
-                MessageBox.Show("The function must have opposite signs at the endpoints of the interval.");
-            }
-
-            while (b - a > eps)
-            {
-                double c = (a + b) / 2;
-                double fc = SolveFunc(func, c.ToString().Replace(",", "."));
-
-                if (Math.Abs(fc) == 0)
-                {
-                    MessageBox.Show($"Ответ: {c}");
-                    return;
-                }
-                else if (fa * fc < 0)
-                {
-                    b = c;
-                }
-                else
-                {
-                    a = c;
-                    fa = fc;
-                }
-            }
-            rtbAnswers.AppendText($"Ответ: {(a + b) / 2}\r\n");
-            MessageBox.Show($"Ответ: {(a + b) / 2}");
-            return;
-        }
-
-        public double SolveFunc(Function function, string x)
-        {
-            return new Expression($"f({x})", function).calculate();
-        }
+    private void расчетыToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        DichotomyMethod();
+      }
+      catch (FormatException)
+      {
+        MessageBox.Show("Please enter valid numbers.");
+      }
 
     }
+
+    private void посторитьГрафикToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        GetFunction(tbFunc.Text);
+      }
+      catch (FormatException)
+      {
+        MessageBox.Show("Please enter valid numbers.");
+      }
+    }
+  }
 }
